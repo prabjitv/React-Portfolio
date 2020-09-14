483a66076a87f9094494c81572d4f9c9a35cd4cf
prab <prabjitv@gmail.com>
2020-09-14 10:28:17 -0700

    rid of navigation

--- src/App.js
+++ src/App.js
@@ -47,9 +47,9 @@ class App extends React.Component {
               <Navbar.Toggle aria-controls="navbar-toggle" className="border-0" />
               <Navbar.Collapse id="navbar-toggle">
                 <Nav className="ml-auto">
-                  <Link className="nav-link" to="/">Home</Link>
+                  {/* <Link className="nav-link" to="/">Home</Link>
                   <Link className="nav-link" to="/about">About</Link>
-                  <Link className="nav-link" to="/contact">Contact</Link>
+                  <Link className="nav-link" to="/contact">Contact</Link> */}
                 </Nav>
               </Navbar.Collapse>
             </Navbar>
